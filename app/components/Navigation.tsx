f7bbafa9f0416d1eaeaa889826953d7101380032
k09 <1416648+k09@users.noreply.github.com>
2024-07-05 23:57:42 +0000

    Added logo and top navigation bar

---
+++ app/components/Navigation.tsx
@@ -0,0 +1,19 @@
+
+export default function Navigation() {
+    return (
+      <>
+        <header className="header">
+            <h1 className="header_logo"><a href="#"><img src="../public/bonus.webp" alt="low-go!" /></a></h1>
+            <nav className="header_nav">
+                <ul className="header_ul">
+                    <li><a href="#" className="header_a">One</a></li>
+                    <li><a href="#" className="header_a">Two</a></li>
+                    <li><a href="#" className="header_a">Three</a></li>
+                    <li><a href="#" className="header_a">Four</a></li>
+                </ul>
+            </nav>
+        </header>
+      </>
+    );
+  }
+  
