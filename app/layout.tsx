d1ffd1fb2be082b21b96aaacc1fad62d78c5eefc
k09 <1416648+k09@users.noreply.github.com>
2024-07-06 17:38:38 +0000

    .....

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navigation from "./components/Navigation";
+import Empty from "./components/Empty";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav><Navigation></Navigation></nav>
-        <main>{children}</main>
+        <Empty/>
+        <main>{
+          children}
+        </main>
       </body>
     </html>
   );
